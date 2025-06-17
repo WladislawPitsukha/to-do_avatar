a53b33c48ada84bace2119faece33a2212203397
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-06-17 23:56:22 +0200

    fix: start to build components to see how looks like

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,1 +1,17 @@
+"use clients";
+
+import { todoTasks } from "@/app/constants/todos";
+import { Todo } from "@/types/todo";
 //TODO: add logic, all elements and make component
+
+import { useState } from "react";
+
+export default function TodoList() {
+    const [listTodos, setListTodos] = useState<Todo[]>(todoTasks);
+
+    return(
+        <section>
+            
+        </section>
+    )
+}
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,1 +1,30 @@
-//TODO: make type for todo
+export type Todo = {
+    main: Todo_main;
+    status: Todo_status;
+    time: Todo_time;
+    extra?: Todo_extra;
+}
+
+type Todo_main = {
+    id: number;
+    title: string;
+    description?: string;
+}
+
+type Todo_status = {
+    completed: boolean;
+    priority: "low" | "medium" | "high";
+    type: "task" | "event" | "note";
+    archived?: boolean;
+}
+
+type Todo_time = {
+    createdAt: Date;
+    updatedAt: Date;
+    dueDate: Date;
+}
+
+type Todo_extra = {
+    tags?: string[];
+    subTasks?: Todo[];
+}
