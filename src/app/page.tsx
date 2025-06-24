b4a51a3e7c138b1615b457d7aa866dbf5ae4c647
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-06-24 09:50:51 +0200

    fix: delete useless file & change tasks & add updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 //TODO: make the main page of the app
 
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import TodoList from "@/components/todoList";
+
 export default function Home() {
   return (
     <div>
-      
+      <Navbar />
+      <TodoList />
+      <Footer />
     </div>
   );
 }
--- src/components/todoItem.tsx
+++ src/components/todoItem.tsx
@@ -1,9 +1,23 @@
 //TODO: add logic, all elements and make component
 
+import { Checkbox } from "@mui/material";
+
 export default function TodoItem() {
     return(
-        <article className="">
+        <article className="rounded-3xl w-full h-[100px] border border-black">
+            <Checkbox />
+            <div className="flex flex-col items-start justify-between">
 
+                <div className="flex items-center justify-between">
+
+                </div>
+                <div className="flex items-center justify-between">
+
+                </div>
+                <div className="">
+
+                </div>
+            </div>
         </article>
     )
 }
