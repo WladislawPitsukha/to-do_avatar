7823a41dfc7efdb94b82417f78809667c1104530
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-06-24 09:51:05 +0200

    feat: add new components

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+//TODO: add logic, all elements and make component
+
+export default function Footer() {
+    return(
+        <footer className="border bg-white border-black w-full h-[90px]">
+
+        </footer>
+    )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+//TODO: add logic, all elements and make component
+
+export default function Navbar() {
+    return(
+        <nav className="border bg-white border-black w-full h-[60px]">
+
+        </nav>
+    )
+}
