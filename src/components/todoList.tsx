5b6a8c7b703a0be1641c70f8876fd8aefcea2168
Wladislaw Pitsukha <70092332+WladislawPitsukha@users.noreply.github.com>
2025-07-14 09:53:17 +0200

    fix: delete mistakes, change a little structure of components, add some tasks for tesing & working

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -9,10 +9,10 @@ import TodoItem from "./todoItem";
 
 export default function TodoList() {
     const [listTodos, setListTodos] = useState<Todo[]>(todoTasks);
-    //TODO: add bytton Speed Dial for choose - chat with bot or add new task(form) 
+    //TODO: add button Speed Dial to choose - chat with bot or add new task(form) 
     //TODO: add logic for add new task, delete task, change status of task
     return(
-        <section className="flex flex-col bg-white h-[500px] w-full gap-3">
+        <section className="flex flex-col bg-white h-auto w-full gap-3">
             {listTodos.map((obj, index) => (
                 <TodoItem 
                     key={index}
