5933e004c0dfc9b8c340c7a41469951ac729867b
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-07-16 11:01:00 +0200

    fix: add working logic for api func to create POST-func, add UI-components for botWidget

--- src/components/botWidget.tsx
+++ src/components/botWidget.tsx
@@ -1,4 +1,6 @@
-//TODO: add logic, all elements and make component
+import { JSX } from "@emotion/react/jsx-runtime";
+import PortraitIcon from '@mui/icons-material/Portrait';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
 
 interface BotWidgetProps {
     message: string;
@@ -6,11 +8,21 @@ interface BotWidgetProps {
     timestamp: Date;
 }
 
+export function CreateIcon(icon: any): JSX.Element {
+    const Icon = icon
+
+    return(
+        <Icon 
+            className="w-[15px] h-[15px] text-white "
+        />
+    )
+}
+
 const BotWidget: React.FC<BotWidgetProps> = ({ message, sender, timestamp }) => {
     const isBot = sender === 'bot';
 
     return(
-        <div className={`flex ${isBot ? 'justify-start' : 'justify-end'}`}>
+        <div className={`flex ${isBot ? 'justify-start' : 'justify-end'} w-auto h-auto`}>
             <div
                 className={`max-w-[70%] rounded-lg p-4 ${
                 isBot
@@ -18,9 +30,9 @@ const BotWidget: React.FC<BotWidgetProps> = ({ message, sender, timestamp }) => 
                     : 'bg-blue-500 text-white'
                 }`}
             >
-                <div className="flex flex-col">
+                <div className="flex flex-col items-start">
                 <span className="text-sm font-medium mb-1">
-                    {isBot ? 'Bot' : 'You'}
+                    {isBot ? <>{CreateIcon(SmartToyIcon)}: Bot</> : <>{CreateIcon(PortraitIcon)}: You</>}
                 </span>
                 <p className="text-base">{message}</p>
                 <span className="text-xs mt-2 opacity-75">
